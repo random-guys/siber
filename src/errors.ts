59ba3e0dddeaa263ce82e1f07bd1a297cc49b91a
Olakunle Arewa <arewa.olakunle@gmail.com>
2019-09-29 18:22:30 +0100

    feat: Added support for 502 and 504 errors closes #12

--- src/errors.ts
+++ src/errors.ts
@@ -5,15 +5,7 @@ export interface HttpError {
 }
 
 export interface HttpDataError extends HttpError {
-  readonly data: any
-}
-
-export class ServerError extends Error implements HttpError {
-  code: number;
-  constructor(message: string) {
-    super(message)
-    this.code = HttpStatus.INTERNAL_SERVER_ERROR
-  }
+  readonly data: any;
 }
 
 export class ControllerError extends Error implements HttpError {
@@ -23,28 +15,51 @@ export class ControllerError extends Error implements HttpError {
   }
 }
 
-export class ActionNotAllowedError extends ControllerError implements HttpError {
+export class ServerError extends ControllerError {
+  code: number;
+  constructor(message: string) {
+    super(message);
+    this.code = HttpStatus.INTERNAL_SERVER_ERROR;
+  }
+}
+
+export class ActionNotAllowedError extends ControllerError {
   constructor(message: string) {
     super(message);
     this.code = HttpStatus.BAD_REQUEST;
   }
 }
 
-export class NotFoundError extends ControllerError implements HttpError {
+export class BadGatewayError extends ControllerError {
+  constructor(message: string) {
+    super(message);
+    this.code = HttpStatus.BAD_GATEWAY;
+  }
+}
+
+export class UnavailableGatewayError extends ControllerError {
+  constructor(message: string) {
+    super(message);
+    this.code = HttpStatus.GATEWAY_TIMEOUT;
+  }
+}
+
+export class NotFoundError extends ControllerError {
   constructor(message: string) {
     super(message);
     this.code = HttpStatus.NOT_FOUND;
   }
 }
 
-export class ConstraintError extends ControllerError implements HttpError {
+export class ConstraintError extends ControllerError {
   constructor(message: string) {
     super(message);
     this.code = HttpStatus.UNPROCESSABLE_ENTITY;
   }
 }
 
-export class ConstraintDataError extends ControllerError implements HttpDataError {
+export class ConstraintDataError extends ControllerError
+  implements HttpDataError {
   data: any;
   code = HttpStatus.UNPROCESSABLE_ENTITY;
   constructor(message: string, data: any) {
