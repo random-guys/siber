d551a919cde45150121bb84894258e4f7b445e4e
Olakunle Arewa <arewa.olakunle@gmail.com>
2019-10-29 22:39:41 +0100

    fix: keys returned by autoloadEnv are upper case

--- src/env.ts
+++ src/env.ts
@@ -1,10 +1,15 @@
 import { MongoConfig } from '@random-guys/bucket';
 import dotenv from 'dotenv';
+import joi from '@hapi/joi';
+import mapKeys from 'lodash/mapKeys';
 
 dotenv.config();
 
 export function autoloadEnv<T>(...keys: string[]): T {
   const envObject = dotenv.config();
+
   // @ts-ignore
-  return envObject.parsed;
+  return mapKeys(envObject.parsed, (_, key) => {
+    return key.toLowerCase();
+  });
 }
