8fc0151303bb5b764a4474d2ba42afbadf3064eb
Olakunle Arewa <arewa.olakunle@gmail.com>
2020-01-06 04:23:30 +0100

    ref: there's no need to interpret Iris errors in siber

--- src/errors.ts
+++ src/errors.ts
@@ -78,29 +78,6 @@ export class ConflictError extends ControllerError {
 }
 
 /**
- * Get the HTTP code for an error that has a prop `code`,or is a `bucket`error, or
- * a `iris` error. Returns `500` otherwise
- * @param err error to parse
- */
-export function isInvalidHTTPCode(err: any) {
-  // check if error code exists and is a valid HTTP code.
-  if (err.code < 100 || err.code > 600) return err.code;
-
-  // integration with bucket
-  if (err instanceof ModelNotFoundError) return HttpStatus.NOT_FOUND;
-  if (err instanceof DuplicateModelError) return HttpStatus.CONFLICT;
-
-  // integration with iris
-  if (err instanceof IrisAPIError) return err.data.code;
-  if (err instanceof IrisServerError)
-    return /timeout/.test(err.message)
-      ? HttpStatus.GATEWAY_TIMEOUT
-      : HttpStatus.BAD_GATEWAY;
-
-  return HttpStatus.INTERNAL_SERVER_ERROR;
-}
-
-/**
  * A global error handler for an entire app. If the error passed is an instance
  * of `ControllerError` it will return its correpsonding code, otherwise it returns
  * `500` and logs the original error
@@ -130,16 +107,6 @@ export function universalErrorHandler(
         "We are having internal issues. Please bear with us",
         HttpStatus.INTERNAL_SERVER_ERROR
       );
-    }
-
-    if (err instanceof IrisAPIError) {
-      err.data = err.data.data;
-      err.message = err.data.message;
-    }
-
-    if (err instanceof IrisServerError || code === 500) {
-      // err.original_message = err.message;
-      err.message = "We are having internal issues. Please bear with us";
     }
 
     res.jSend.error(err["data"], err.message, err.code);
