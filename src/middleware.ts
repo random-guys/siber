7e577dc502b4f6347f1e78e4885884c432d0dedb
Olakunle Arewa <arewa.olakunle@gmail.com>
2020-03-22 11:51:22 +0100

    ref: nesting excludeAgents inside tracking is redundant

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,10 +9,16 @@ const kubernetesAgents = [/kube-probe/i, /Prometheus/i];
 
 export interface SiberConfig {
   cors: boolean | CorsOptions;
-  log_kubernetes?: boolean;
-  tracking: {
-    excludeAgents?: RegExp[];
-  };
+  /**
+   * whether or not to log kube-proxy and prometheus user agents. Defaults
+   * to false.
+   */
+  logKubernetes?: boolean;
+  /**
+   * agents to ignore when logging requests. Excludes kube-probe and Prometheus
+   * unless `logKubernetes` is true.
+   */
+  excludeAgents?: RegExp[];
 }
 
 export function build(app: Application, logger: Logger, conf: SiberConfig) {
@@ -21,12 +27,12 @@ export function build(app: Application, logger: Logger, conf: SiberConfig) {
   app.use(express.urlencoded({ extended: false }));
   app.use(refreshJSend);
 
-  conf.tracking.excludeAgents = conf.tracking.excludeAgents ?? [];
-  if (!conf.log_kubernetes) {
-    conf.tracking.excludeAgents.push(...kubernetesAgents);
+  conf.excludeAgents = conf.excludeAgents ?? [];
+  if (!conf.logKubernetes) {
+    conf.excludeAgents.push(...kubernetesAgents);
   }
 
-  app.use(requestTracker(logger, conf.tracking.excludeAgents));
+  app.use(requestTracker(logger, conf.excludeAgents));
   app.use(responseTime());
 
   // CORS
