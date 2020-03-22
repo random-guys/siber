71decf2e029c7f44abcc4949ca39fc5860ba095d
Olakunle Arewa <arewa.olakunle@gmail.com>
2020-03-22 11:45:05 +0100

    feat: adding kube-proxy and prom to all my service configs is stress
    Since logging is turned on by default, the logs would get chatty
    and full quickly if deployed with kubernetes since prometheus and
    kube-proxy query the service often. So I added their agents to
    the agent exclusion list by default, while giving users the ability
    to disable that using `log_kubernetes`

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,8 +5,11 @@ import responseTime from "response-time";
 import { requestTracker } from "./logging";
 import { refreshJSend } from "./jsend";
 
+const kubernetesAgents = [/kube-probe/i, /Prometheus/i];
+
 export interface SiberConfig {
   cors: boolean | CorsOptions;
+  log_kubernetes?: boolean;
   tracking: {
     excludeAgents?: RegExp[];
   };
@@ -17,6 +20,12 @@ export function build(app: Application, logger: Logger, conf: SiberConfig) {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(refreshJSend);
+
+  conf.tracking.excludeAgents = conf.tracking.excludeAgents ?? [];
+  if (!conf.log_kubernetes) {
+    conf.tracking.excludeAgents.push(...kubernetesAgents);
+  }
+
   app.use(requestTracker(logger, conf.tracking.excludeAgents));
   app.use(responseTime());
 
